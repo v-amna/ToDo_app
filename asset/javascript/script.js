bc7e8022993f932fdf0d52eae49567170f81745c
v-amna <amnamampad@gmail.com>
2026-01-01 20:25:19 +0100

    add user task

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -2,13 +2,49 @@ let task=[];
 const addTask = ()=> {
     const taskInput= document.getElementById('taskInput')
     const text= taskInput.value.trim()
-    if(text){
-        task.push({text:text, completed:false});
+    if(text===""){
+       
+        
+        alert("Please enter a task");
+        return;
     }
-    console.log(task);
+     task.push({
+        text:text, 
+        completed:false
+    });
+     taskInput.value="";
+     updateTasksList();
 
 };
+const updateTasksList = () => {
+    const tasksList= document.getElementById('task-list');
+    tasksList.innerHTML = ""
+
+    task.forEach((item, Index) => {
+
+        const listItem= document.createElement("li");
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task">
+             <input type="checkbox" ${item.completed ? "checked" : ""} 
+                        onclick="toggleTask(${Index})">
+                    <p class="${item.completed ? "completed" : ""}">
+                        ${item.text}
+                    </p>
+                </div>
+                <div class="icons">
+                    <button onclick="deleteTask(${index})">🗑</button>
+                </div>
+            </div>   
+
+        `;
+        tasksList.appendChild(listItem);
+    });
+
+};
+
+
 document.getElementById("newTask").addEventListener('click', function(e){
-    e.preventDefault();
+   e.preventDefault();
     addTask();
 })
